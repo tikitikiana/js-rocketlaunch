d55f1cb76b631309f8bfc134cb3f602c40bf52a4
tikitikiana <tzdravic@gmail.com>
2015-12-26 05:51:08 +0200

    Made Some Car Stuff Happen!

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var car = {
+			make: 'VW',
+			type: 'Polo',
+			color: 'blue',
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats: [
+				'seat 1', 
+				'seat2', 
+				'seat3', 
+				'seat4'
+				],
+			turnOn: function () {
+				this.isTurnedOn = true;
+			},
+			fly: function () {
+				alert('fly');
+			},
+			switchCar: function (isOn) {
+				console.log('turn car ' + isOn)
+				if (isOn == true) {
+					this.isTurnedOn = true;
+				} else {
+					this.isTurnedOn = false;
+				}
+
+			}
+		};
+ 	console.log('hello there friends!');
