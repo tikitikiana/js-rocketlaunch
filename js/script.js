5d442af66f686d897af08de789b4bb8b7cbcc9a6
tikitikiana <tzdravic@gmail.com>
2015-12-26 06:16:36 +0200

    Coolness

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+// hey this is a comment
+/* hey this function 
+changes the class name
+from the id to the assigned class name
+*/
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+
+	if (currentClassName == 'cool') {
+		document.getElementById ('cool').className = 'cool red';
+
+	} else {
+		document.getElementById('cool').className = 'cool';
+
+	}
+}
 var car = {
 			make: 'VW',
 			type: 'Polo',
