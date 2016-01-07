33f8fbb3d9f7364345114a04a8d7347cb64e8d95
tikitikiana <tzdravic@gmail.com>
2016-01-07 17:51:33 +0200

    Completed Project!!!

--- js/rocket.js
+++ js/rocket.js
@@ -1,38 +1,1 @@
-var timer = null;
-var countdownNumber=10;
-
-var changeState = function (state) {
-	document.body.className = 'body-state' + state;
-	clearInterval(timer);
-	countdownNumber = 10;
-	document.getElementById('countdown').innerHTML = countdownNumber;
-
-	//countdown
-	if (state == 2) {
-		timer = setInterval (function () {
-			countdownNumber = countdownNumber-1;
-			document.getElementById('countdown').innerHTML = countdownNumber; 
-			if (countdownNumber <=0) {
-				changeState(3);
-			};
-	}, 500);
-	} else if (state == 3) {
-		var success = setTimeout( function () 
-		{
-			var randomNumber = Math.round(Math.random()*10);
-			console.log('randomNumber:', randomNumber)
-			//success
-			if (randomNumber > 5) {
-				changeState(4); //success
-			}
-			else{
-				changeState(5); //failure
-			}
-
-		}, 2000);
-
-	};
-}
-
-
-
+var timer = null;var countdownNumber=10;var changeState = function (state) {	document.body.className = 'body-state' + state;	clearInterval(timer);	countdownNumber = 10;	document.getElementById('countdown').innerHTML = countdownNumber;	//countdown	if (state == 2) {		timer = setInterval (function () {			countdownNumber = countdownNumber-1;			document.getElementById('countdown').innerHTML = countdownNumber; 			if (countdownNumber > 1 && countdownNumber <=4) {				 //can't wait				 document.getElementById('cant-wait').className = 'cant-wait show';			} else{				document.getElementById('cant-wait').className = 'cant-wait';			}			if (countdownNumber > 4 && countdownNumber <= 7) {				 //be nervous				 document.getElementById('nervous').className = 'nervous show';			} else {				document.getElementById('nervous').className = 'nervous';			} 						if (countdownNumber <=0) {				changeState(3);			};	}, 500);	} else if (state == 3) {		var success = setTimeout( function () 		{			var randomNumber = Math.round(Math.random()*10);			console.log('randomNumber:', randomNumber)			//success			if (randomNumber > 5) {				changeState(4); //success			}			else{				changeState(5); //failure			}		}, 2000);	};}
--- js/script.js
+++ js/script.js
@@ -1,45 +1,1 @@
-// hey this is a comment
-/* hey this function 
-changes the class name
-from the id to the assigned class name
-*/
-var doCoolStuff = function () {
-	var currentClassName = document.getElementById('cool').className;
-
-	if (currentClassName == 'cool') {
-		document.getElementById ('cool').className = 'cool red';
-
-	} else {
-		document.getElementById('cool').className = 'cool';
-
-	}
-}
-var car = {
-			make: 'VW',
-			type: 'Polo',
-			color: 'blue',
-			isTurnedOn: false,
-			numberOfWheels: 4,
-			seats: [
-				'seat 1', 
-				'seat2', 
-				'seat3', 
-				'seat4'
-				],
-			turnOn: function () {
-				this.isTurnedOn = true;
-			},
-			fly: function () {
-				alert('fly');
-			},
-			switchCar: function (isOn) {
-				console.log('turn car ' + isOn)
-				if (isOn == true) {
-					this.isTurnedOn = true;
-				} else {
-					this.isTurnedOn = false;
-				}
-
-			}
-		};
- 	console.log('hello there friends!');
+// hey this is a comment/* hey this function changes the class namefrom the id to the assigned class name*/setTimeout(function() {	location.href='rocket.html';	}, 2000);var doCoolStuff = function () {	var currentClassName = document.getElementById('cool').className;	if (currentClassName == 'cool') {		document.getElementById ('cool').className = 'cool red';	} else {		document.getElementById('cool').className = 'cool';	}}var car = {			make: 'VW',			type: 'Polo',			color: 'blue',			isTurnedOn: false,			numberOfWheels: 4,			seats: [				'seat 1', 				'seat2', 				'seat3', 				'seat4'				],			turnOn: function () {				this.isTurnedOn = true;			},			fly: function () {				alert('fly');			},			switchCar: function (isOn) {				console.log('turn car ' + isOn)				if (isOn == true) {					this.isTurnedOn = true;				} else {					this.isTurnedOn = false;				}			}		}; 	console.log('hello there friends!');
